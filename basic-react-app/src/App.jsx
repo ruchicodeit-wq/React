196a21c4437702ba04ffbc16428e69d01750e707
ruchicodeit-wq <ruchicodeit@gmail.com>
2026-08-17 23:42:12 +0530

    initial commit

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import MsgBox from "./MsgBox.jsx"
+
+function App() {
+
+  return (
+    <>
+    <MsgBox userName="Ruchi" textColor="Pink"/>
+    </>
+  );
+
+}
+
+export default App;
